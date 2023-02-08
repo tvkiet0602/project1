a5f33141d268f3a3e87ca0b3609825b6a8c32fa2
tvkiet0602 <tvkiet@ninepoints.vn>
2023-02-08 18:10:07 +0700

    fix index

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
+<?php
+    session_start();
+    if(!isset($_SESSION["username"])){
+        header("location: ../login.php");
+    }
+    header("Content-Type: text/html; charset-UTF-8");
+    include 'connect.php';
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -30,7 +39,7 @@
         #row .col-sm-2{
             width: 20em;
             
-        }
+
         
     </style>
 </head>
@@ -40,12 +49,6 @@
         <nav class="navbar navbar-default navbar-fixed-top">
             <div class="container-fluid">
               <div class="navbar-header">
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-                  <span class="sr-only">Toggle navigation</span>
-                  <span class="icon-bar"></span>
-                  <span class="icon-bar"></span>
-                  <span class="icon-bar"></span>
-                </button>
                 <a style="color: #007bff; font-size:xx-large;" class="navbar-brand" href="#">MyBlog</a>
               </div>
               <div id="navbar" class="navbar-collapse collapse">
@@ -67,58 +70,103 @@
     <br><br><br>
 <!--Container-->
 <div id="row">
-    <div class="col-md-8 col-md-offset-1" style="background-color: #c4b4c1; height: auto;">
-       
-            <h2  style="text-align: center;">Bài viết mới</h2>
-     
+    <div class="col-md-7 col-md-offset-1" style="background-color: #c4b4c1; height: auto;">
+
+        <button style="margin: 50px 80px 0px; type="button" class="btn btn-primary">BÀI VIẾT MỚI</button>
+<!--        --><?php
+//            $result = mysqli_query($con, "SELECT * FROM blog_posts order by post_date DESC ") or die ("Lỗi hiển thị");
+//            while ($r = mysqli_fetch_array($result)){
+//                echo "<h3>".$r["title"]."</h3>";
+//                echo "<h5><em>Cập nhật: ".$r["post_date"]."</em></h5>";
+//                echo "<h5><em>Người tạo: ".$r["username"]."</em></h5>";
+//                echo "<p>".$r["content"]."</p>";
+//                echo "<p><a class='btn btn-default' href='#' role='button'>Xem chi tiết &raquo;</a></p>";
+//            }
+//        ?>
         <div class="container">
             <!-- Example row of columns -->
             <div>
-                <div class="col-xs-6 col-md-4" >
-                  <h3>Heading</h3>
-                  <h5><em>Cập nhật: 30/01/2023</em></h5>
-                  <h5><em>Người tạo: Trần Văn Kiệt</em></h5>
-                  <!-- <img src="./assets/css/img/avatar1.jpg" alt=""> -->
+                <div class="col-xs-11" >
+                    <?php
+                    $result= mysqli_query($con, "SELECT * FROM blog_posts order by post_date DESC ") or die ("Lỗi hiển thị");
+                    $r = mysqli_fetch_array($result);
+//                        echo "<h3>".$r["title"]."</h3>";
+                        echo "<table  style='width: 80%; margin-left: 50px;' > 
+                                <tr>
+                                    <td rowspan='4'>
+                                        <img src='https://sachhay24h.com/uploads/news/a_1969152847_giaithichcautucngumotconnguadaucatauboco1.jpeg' alt='...' style='width: 200px; height: 150px;'>
+                                    </td>
+                               
+                                     <td style='float: left;margin-bottom: -10px;'>
+                                        <p><b style='font-size: 20px;'>".$r["title"]."</b></p><br>
+                                    </td>
+                                </tr>
+                                <tr>
+                                 <td style='margin-bottom: -10px;'>
+                                        <h5><em>Ngày đăng: ".$r["post_date"]." --- Người tạo: ".$r["username"]."</em></h5><em></td>
+                                </tr>
+                                <tr>
+                                    <td style='float: left;'>
+                                        <h5>";
+                                if(strlen($r["content"]) > 100)
+                                    $cOutput = mb_substr($r["content"], 0, 99, "UTF-8");
+                                    echo $cOutput . "...";
+                                    echo "</h5>
+                                                </td>
+                                                <td> </td><td> </td>
+                                      
+                                                
+                                            </tr>
+                                            <tr>
+                                            <td>
+                                            <p><a class='btn btn-default' href='#' role='button'>Xem chi tiết &raquo;</a></p>
+</td>
+</tr>";
 
-                  <p >Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-                  <p><a class="btn btn-default" href="#" role="button">Xem chi tiết &raquo;</a></p>
-                </div>
-                <div class="col-xs-6 col-md-4" style="margin-left: 50px;" >
-                  <h3>Heading</h3>
-                  <h5><em>Cập nhật: 30/01/2023</em></h5>
-                  <h5><em>Người tạo: Trần Văn Kiệt</em></h5>
-                  <!-- <img src="./assets/css/img/4.jpg" alt=""> -->
-                  <p >Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-                  <p><a class="btn btn-default" href="#" role="button">Xem chi tiết &raquo;</a></p>
-               </div>
-            </div><br><br>
-            <div>
-                <div class="col-xs-6 col-md-4" >
-                    <h3>Heading</h3>
-                    <h5><em>Cập nhật: 30/01/2023</em></h5>
-                  <h5><em>Người tạo: Trần Văn Kiệt</em></h5>
-                    <p >Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-                    <p><a class="btn btn-default" href="#" role="button">Xem chi tiết &raquo;</a></p>
-                  </div>
-                  <div class="col-xs-6 col-md-4" style="margin-left: 50px;" >
-                    <h3>Heading</h3>
-                    <h5><em>Cập nhật: 30/01/2023</em></h5>
-                  <h5><em>Người tạo: Trần Văn Kiệt</em></h5>
-                    <p >Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
-                    <p><a class="btn btn-default" href="#" role="button">Xem chi tiết &raquo;</a></p>
-                 </div>
-            </div>
-        </div>
-    </div>
 
 
-    <div class="col-sm-2"  style="background-color: #c4b4c1; float:right;">
-            <h3 style="margin-top: 80px;" >Người dùng</h3>
-            <p>
-                <img src="assets/css/img/avatar2.jpg" alt="..." class="img-circle"><a style="text-decoration: none; color: black;">&emsp;Trần Văn Kiệt</a>
-            </p>
-       
-    </div>
+//                        echo "<h5><em>Cập nhật: ".$r["post_date"]."</em></h5>";
+//                        echo "<h5><em>Người tạo: ".$r["username"]."</em></h5>";
+//                        echo "<p>".$r["content"]."</p>";
+//                        echo "<p><a class='btn btn-default' href='#' role='button'>Xem chi tiết &raquo;</a></p>";
+
+                    ?>
+<!--                  <h3>--><?php //echo ""; ?><!--</h3>-->
+<!--                  <h5><em>Cập nhật: 30/01/2023</em></h5>-->
+<!--                  <h5><em>Người tạo: Trần Văn Kiệt</em></h5>-->
+<!--                  <!-- <img src="./assets/css/img/avatar1.jpg" alt=""> -->-->
+<!---->
+<!--                  <p >Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>-->
+<!--                  <p><a class="btn btn-default" href="#" role="button">Xem chi tiết &raquo;</a></p>-->
+<!--                </div>-->
+<!--              <br><br>-->
+<!--            <div>-->
+<!--                <div class="col-xs-6 col-md-4" >-->
+<!--                    <h3>Heading</h3>-->
+<!--                    <h5><em>Cập nhật: 30/01/2023</em></h5>-->
+<!--                  <h5><em>Người tạo: Trần Văn Kiệt</em></h5>-->
+<!--                    <p >Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>-->
+<!--                    <p><a class="btn btn-default" href="#" role="button">Xem chi tiết &raquo;</a></p>-->
+<!--                  </div>-->
+<!--                  <div class="col-xs-6 col-md-4" style="margin-left: 50px;" >-->
+<!--                    <h3>Heading</h3>-->
+<!--                    <h5><em>Cập nhật: 30/01/2023</em></h5>-->
+<!--                  <h5><em>Người tạo: Trần Văn Kiệt</em></h5>-->
+<!--                    <p >Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>-->
+<!--                    <p><a class="btn btn-default" href="#" role="button">Xem chi tiết &raquo;</a></p>-->
+<!--                 </div>-->
+<!--            </div>-->
+<!--        </div>-->
+<!--    </div>-->
+
+<!---->
+<!--    <div class="col-sm-2"  style="background-color: #c4b4c1; float:right;">-->
+<!--            <h3 style="margin-top: 80px;" >Người dùng</h3>-->
+<!--            <p>-->
+<!--                <img src="assets/css/img/avatar2.jpg" alt="..." class="img-circle"><a style="text-decoration: none; color: black;">&emsp;Trần Văn Kiệt</a>-->
+<!--            </p>-->
+<!--       -->
+<!--    </div>-->
 </div>
 </body>
 </html>
