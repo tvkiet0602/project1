41eefdb9b3da3fa520e8eba254a9f497d01ca466
tvkiet0602 <tvkiet@ninepoints.vn>
2023-02-08 11:20:12 +0700

    create detail, post and update index

--- post.php
+++ post.php
@@ -1,3 +1,31 @@
+<?php
+    session_start();
+    if(!isset($_SESSION["username"])){
+        header("loation: ./login.php");
+    }
+    header("Content-Type: text/html; charset-UTFF-8");
+    if(!isset($con)){
+        include 'connect.php';
+    }
+    $ID = $_GET['id'];
+    $qr = mysqli_query($con, "SELECT * FROM users WHERE user_id='$ID'") or die("Lỗi truy vấn");
+    $r = mysqli_fetch_array($qr);
+
+    $user_id = $_POST['user_id'];
+    $title = $_POST['title'];
+    $content = $_POST['content'];
+    $image_url = $_POST['image_url'];
+    $is_private = $_POST['is_private'];
+    $post_date = $_POST['post_date'];
+    if(isset($_SESSION["username"])){
+        $user_id = mysqli_fetch_array(mysqli_query($con, "SELECT * FROM users WHERE user_id = '$user_id'") or die ("Lỗi truy vấn user_id"));
+        $user_id = $user_id['ID'];
+        $result = mysqli_query($con, "INSERT INTO blog_posts(user_id, title, content, image_url, is_private, post_date) 
+            VALUES ('$user_id', '$title', '$content' '$image_url', '$is_private', '$post_date')") or die ("Lỗi truy vấn blog_posts");
+    }
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -68,7 +96,7 @@
 
     <!--Container-->
     <div id="container" >
-        <form action=".php" method="POST">
+        <form action="post.php" method="POST">
             <div class="row" style="width: auto;">
                 <div class="col-md-6 col-md-offset-3">
                     <div class="panel panel-primary" style="margin-left: 10px;">
@@ -100,7 +128,17 @@
                                 <div class="col-sm-12">
                                     <dl>
                                         <dd>
-                                            <textarea class="form-control" rows="7" name="noidung" placeholder="Bạn đang muốn chia sẻ điều gì?"></textarea>
+                                            <input type="text" class="form-control" name="title" placeholder="Nhập tiêu đề bài viết" required>
+                                        </dd>
+                                    </dl>
+                                    <dl>
+                                        <dd>
+                                            <textarea class="form-control" rows="7" name="content" placeholder="Bạn đang muốn chia sẻ những gì?" required></textarea>
+                                        </dd>
+                                    </dl>
+                                    <dl>
+                                        <dd>
+                                            <input type="file"  id="file-upload" />
                                         </dd>
                                     </dl>
                                 </div>
